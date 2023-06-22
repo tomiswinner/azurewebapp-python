ba7a084a1d5dab5c3f1e0c5cd6520093d4124f25
omiswinner <mouseofsack911@gmail.com>
2023-06-22 23:02:02 +0900

    add environ

--- application.py
+++ application.py
@@ -1,20 +1,22 @@
 
 # コードソース　https://qiita.com/y_a_m_a/items/2fef42212f5ffeb11531
 
-from flask import Flask,render_template,request
+
+import os
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def main_page():
     if request.method == 'GET':
-        text = "ここに結果が出力されます"
-        return render_template("page.html",text=text)
+        # Use an empty string '' as the default value
+        text = f"ここに結果が出力されます{os.environ.get('KEY_VAULT_TEST1', '')}"
+        return render_template("page.html", text=text)
     elif request.method == 'POST':
         name = request.form["name"]
         text = "こんにちは" + name + "さん"
-        return render_template("page.html",text=text)
+        return render_template("page.html", text=text)
 
-## 実行
 if __name__ == "__main__":
     app.run(debug=True)
